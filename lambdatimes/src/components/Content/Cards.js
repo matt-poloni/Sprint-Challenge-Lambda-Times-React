6a072608c8a08758b5c8569198c52891bb7cfc61
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-29 13:03:30 -0400

    Converts Cards.js & Card.js to styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const WrapCards = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {width: 1200px}
+`
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <WrapCards>
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/
         props.cards.map((card, i) =>
@@ -14,7 +27,7 @@ const Cards = props => {
           />
         )
       }
-    </div>
+    </WrapCards>
   )
 }
 
